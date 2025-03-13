75ebfda7dacb05c5c4ad0e76342f1f2d0df8eeea
DQL678 <dungql678@gmail.com>
2025-03-13 15:17:10 +0100

    Funktion

--- sketch.js
+++ sketch.js
@@ -1,6 +1,3 @@
-function setup() {
-  createCanvas(1705, 791);
-}
 let cirkelX = 830;
 let cirkelY = 400;
 let cirkelR = 50;
@@ -11,50 +8,60 @@ let joyY = 0;
 let canvasX = 1705;
 let canvasY = 791;
 
-function draw(){
-  background(200);
-  circle(cirkelX,cirkelY,cirkelR);
-  circle(cirkelX+canvasX, cirkelY,cirkelR);
-  circle(cirkelX-canvasX, cirkelY,cirkelR);
-  circle(cirkelX,cirkelY-canvasY, cirkelR);
-  circle(cirkelX,cirkelY+canvasY, cirkelR);
-  
-  // Bevægelse
-  let joyX = 0;
-  let joyY = 0;
-
- // Bevægelsestaster
- if(keyIsDown(87)){
-  // W eller op
-  joyY = -3;
- }
- if(keyIsDown(83)){
-  // S eller ned
-  joyY = 3;
+function setup() {
+  createCanvas(canvasX, canvasY);
 }
-  if(keyIsDown(65)){
+
+function draw() {
+  background(200);
+  Bevægelse();
+}
+
+function Bevægelse() {
+  // Nulstil bevægelsen for hver frame
+  joyX = 0;
+  joyY = 0;
+
+  // Bevægelsestaster
+  if (keyIsDown(87)) {
+    // W eller op
+    joyY = -5;
+  }
+  if (keyIsDown(83)) {
+    // S eller ned
+    joyY = 5;
+  }
+  if (keyIsDown(65)) {
     // A eller venstre
-    joyX = -3;
+    joyX = -5;
   }
-  if(keyIsDown(68)){
+  if (keyIsDown(68)) {
     // D eller højre
-    joyX = 3;
+    joyX = 5;
   }
+
+  // Opdater position
   cirkelX += joyX;
   cirkelY += joyY;
 
-  // Væggene
-  if(cirkelX > canvasX){
+  // Væggene - wrap around
+  if (cirkelX > canvasX) {
     cirkelX = 0;
   }
-  if(cirkelX < 0){
+  if (cirkelX < 0) {
     cirkelX = canvasX;
   }
-  if(cirkelY > canvasY){
+  if (cirkelY > canvasY) {
     cirkelY = 0;
   }
-  if(cirkelY < 0){
+  if (cirkelY < 0) {
     cirkelY = canvasY;
   }
 
+  // Tegn cirklerne
+  circle(cirkelX, cirkelY, cirkelR);
+  circle(cirkelX + canvasX, cirkelY, cirkelR);
+  circle(cirkelX - canvasX, cirkelY, cirkelR);
+  circle(cirkelX, cirkelY - canvasY, cirkelR);
+  circle(cirkelX, cirkelY + canvasY, cirkelR);
 }
